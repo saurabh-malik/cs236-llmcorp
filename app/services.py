ab78e61438d1057e252257a47a98fc3fbe4ab3d9
saurabh-malik <malik.saurabh@hotmail.com>
2023-11-30 13:39:23 -0800

    Introduce custom prompt to behave agent as Corp Agent named Corpy.

--- app/services.py
+++ app/services.py
@@ -1,6 +1,33 @@
 import torch
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain.schema.messages import AIMessage, HumanMessage
+
 from model.customchain.chains import MyConversationalRetrievalChain
 from model.utils.setup_utils import get_llm, get_vector_db
+
+#ToDo SM (1) - Replace the custom chains with Runnables
+#ToDo SM (2) - Maintain chat history
+
+
+#Prompt for QA Agent
+qa_system_prompt = ("You are a customer support agent in a IT corporate called Dummy Corp, your name is Corp Agent. "
+    "Use the following pieces of context to answer the question at the end. "
+    "If you don't know the answer, just say that you don't know, don't try to make up an answer. "
+    "Use three sentences maximum and keep the answer as concise as possible. "
+    "{context}"
+    "Question: {question}"
+    "Helpful Answer:")
+rag_prompt_custom = PromptTemplate.from_template(qa_system_prompt)
+
+#Prompt for Standalone Question Generation :: Keeping it simple for now.
+saq_base_template = (
+    "Combine the chat history and follow up question into "
+    "a standalone question. Chat History: {chat_history}"
+    "Follow up question: {question}"
+)
+saq_base_prompt = PromptTemplate.from_template(saq_base_template)
 
 
 # Define a service that uses dependencies
@@ -8,9 +35,14 @@ def get_llm_answer(question):
     llm = get_llm()  # Obtain the MyHuggingFacePipeline object from the dependency
     vector_db = get_vector_db()  # Obtain the MyFAISS object from the dependency
 
-    device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
+    # Keeping default search behavior for retriver
+    # ToDo SM (3) - Handle Lost in the middle during retrieval
 
-    chain = MyConversationalRetrievalChain.from_llm(llm.pipeline, vector_db.as_retriever(), return_source_documents=True)
+    retriever = vector_db.as_retriever()
+    #retriever = vector_db.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+
+   
+    chain = MyConversationalRetrievalChain.from_llm(llm.pipeline, retriever, saq_base_prompt, rag_prompt_custom, return_source_documents=True)
     chat_history = []
 
     result = chain({"question": question, "chat_history": chat_history})
