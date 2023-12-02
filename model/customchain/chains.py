d35161629a266f93caf82a4d156af4d630c1b32f
aufish <shenchg126@gmail.com>
2023-12-02 14:09:41 -0800

    [FIX] compatible with the format without system prompot

--- model/customchain/chains.py
+++ model/customchain/chains.py
@@ -1,8 +1,25 @@
 from langchain.chains import ConversationalRetrievalChain
+from langchain.schema import BaseRetriever
+from langchain.schema.language_model import BaseLanguageModel
+
 
 class MyConversationalRetrievalChain(ConversationalRetrievalChain):
     @classmethod
-    def from_llm(cls, llm, retriever, condense_q_prompt, prompt, return_source_documents=True):
-        chain = ConversationalRetrievalChain.from_llm(llm, retriever, condense_question_prompt = condense_q_prompt, return_source_documents=return_source_documents, combine_docs_chain_kwargs={'prompt': prompt})
+    def from_llm(cls, llm: BaseLanguageModel, retriever: BaseRetriever, condense_q_prompt=None, prompt=None,
+                 return_source_documents=True):
+        if condense_q_prompt:
+            chain = ConversationalRetrievalChain.from_llm(
+                llm,
+                retriever,
+                condense_question_prompt=condense_q_prompt,
+                combine_docs_chain_kwargs={'prompt': prompt},
+                return_source_documents=return_source_documents,
+            )
+        else:
+            chain = ConversationalRetrievalChain.from_llm(
+                llm,
+                retriever,
+                return_source_documents=return_source_documents,
+            )
         #chain = ConversationalRetrievalChain.from_llm(llm, retriever, return_source_documents=return_source_documents, combine_docs_chain_kwargs={'prompt': prompt})
         return chain
