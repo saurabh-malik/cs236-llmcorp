aa23239bd156e3d42d5b0c2aff6f90567be7383e
saurabh-malik <malik.saurabh@hotmail.com>
2024-08-05 20:02:52 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class AppConfig:
     #model_id = 'meta-llama/Llama-2-13b-chat-hf'
     model_id = 'meta-llama/Llama-2-7b-chat-hf'
     embedding_model = 'sentence-transformers/all-mpnet-base-v2'
-    hf_auth = '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'
+    hf_auth = '#hf_auth_key'
     crwaler_key = 'apify_api_MgtGtJKQA1NLYDV5rmnivmYx6kphkp1p57LV'
     isCrawlingOpen = False
-    openai_api_key='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'
+    openai_api_key='#Key'
     fine_tuned_model='./model/fine-tuned/llama-2-13b-CorpGL'
     use_custom_model=False
     #Add other configuration settings here
